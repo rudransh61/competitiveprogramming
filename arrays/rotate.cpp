c8070cca0eb81bc4ba8c20630d1c024d492b5d8f
Rudransh <aarti19830@gmail.com>
2022-11-01 16:41:12 +0530

    added some more files dsa lec 23 lgbg

--- arrays/rotate.cpp
+++ arrays/rotate.cpp
@@ -1,47 +1,24 @@
-
-#include <bits/stdc++.h>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void Rotate(int arr[], int d, int n)
+class Solution
 {
-    int temp[n];
-
-    int k = 0;
-
-    for (int i = d; i < n; i++)
+public:
+    void rotate(vector<int> &nums, int k)
     {
-        temp[k] = arr[i];
-        k++;
+        int n = nums.size();
+        vector<int> temp(n);
+
+        for (int i = 0; i < n; i++)
+        {
+
+            temp[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            nums[i] = temp[i];
+        }
     }
-
-    for (int i = 0; i < d; i++)
-    {
-        temp[k] = arr[i];
-        k++;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = temp[i];
-    }
-}
-
-void PrintTheArray(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-}
-
-int main()
-{
-    int arr[] = {1, 2, 3, 4, 5, 6, 7};
-    int N = sizeof(arr) / sizeof(arr[0]);
-    int d = 2;
-
-    Rotate(arr, d, N);
-    PrintTheArray(arr, N);
-
-    return 0;
-}
+};
---
+++ dsa/addarrays.cpp
@@ -0,0 +1,64 @@
+// arrays are digits and we have to add them
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> reverse(vector<int> v)
+{
+
+    int s = 0;
+    int e = v.size() - 1;
+
+    while (s < e)
+    {
+        swap(v[s++], v[e--]);
+    }
+    return v;
+}
+
+vector<int> findArraySum(vector<int> &a, int n, vector<int> &b, int m)
+{
+
+    int i = n - 1;
+    int j = m - 1;
+    vector<int> ans;
+    int carry = 0;
+
+    while (i >= 0 && j >= 0)
+    {
+        int val1 = a[i];
+        int val2 = b[j];
+
+        int sum = val1 + val2 + carry;
+
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        i--;
+        j--;
+    }
+    while (i >= 0)
+    {
+        int sum = a[i] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        i--;
+    }
+    while (j >= 0)
+    {
+        int sum = b[j] + carry;
+        carry = sum / 10;
+        int value = sum % 10;
+        ans.push_back(value);
+        j--;
+    }
+    while (carry != 0)
+    {
+        int sum = carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+    }
+    return reverse(ans);
+}
